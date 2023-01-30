97f38ac4e8d24ff2ba488ad2691576ded484b0c9
Anthony Giordano <anthonygiordano1993@gmail.com>
2023-01-30 16:31:25 +0100

    Commit

---
+++ src/Esercizio.java
@@ -0,0 +1,9 @@
+public class Esercizio {
+
+    /*git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/Anthony1993-cyber/esercizio.git
+    git push -u origin main*/
+}
